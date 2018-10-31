144b222168eab6e9ed950ea5321f52783790fa89
Jamie <jamiesullivan523@gmail.com>
2018-10-31 16:59:19 +0000

    Setup dev environment

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  devServer: {
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
